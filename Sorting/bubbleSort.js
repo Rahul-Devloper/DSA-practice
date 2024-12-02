3d4787fc027f7c6466d900900a4a9e51fa3e3ea4
Rahul-Devloper <rahulramesh26@yahoo.com>
2024-12-02 13:41:16 +0000

    bubble sort in descending order

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -12,22 +12,51 @@
 const array = [-6, 20, 8, -2, 4]
 // first [-6,8,-2,4,20]
 
-function bubbleSort(array) {
-  let swapped
-  let n = array.length
-  do {
-    swapped = false
-    for (let i = 0; i < n - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        let temp = array[i]
-        array[i] = array[i + 1]
-        array[i + 1] = temp
-        swapped = true
-      }
-    }
-    n-- //reducing the iterations as the last element is already sorted and the array lenght can be shortened to focus on remaining elementes
-  } while (swapped)
-  return array
-}
+//to sort in ascending order
+// function bubbleSort(array) {
+//   let swapped
+//   let n = array.length
+//   do {
+//     swapped = false
+//     for (let i = 0; i < n - 1; i++) {
+//       if (array[i] > array[i + 1]) {
+//         let temp = array[i]
+//         array[i] = array[i + 1]
+//         array[i + 1] = temp
+//         swapped = true
+//       }
+//     }
+//     n-- //reducing the iterations as the last element is already sorted and the array lenght can be shortened to focus on remaining elementes
+//   } while (swapped)
+//   return array
+// }
 
-console.log(bubbleSort(array))
+// console.log(bubbleSort(array))
+
+
+//to sort in descending order
+// you can either set ponters for left Index, or just swap the condition inside the if loop.
+function bubbleSort(array) {
+    let swapped
+    // let leftIndex = 0
+    let n = array.length
+
+    do {
+      swapped = false
+      for (let i = 0; i < n - 1; i++) {
+        if (array[i] < array[i + 1]) {
+          let temp = array[i]
+          array[i] = array[i + 1]
+          array[i + 1] = temp
+          swapped = true
+        }
+      }
+    //   leftIndex ++ //reducing the iterations as the last element is already sorted and the array lenght can be shortened to focus on remaining elementes
+    n--
+    } while (swapped)
+    return array
+  }
+  
+  console.log(bubbleSort(array))
+  
+  
